97580cf2cf7cae856b72db200802924c412449b9
maryGarL <maryel_565@hotmail.com>
2017-06-13 17:48:18 -0500

    login

---
+++ js/utils/facebook.js
@@ -0,0 +1,33 @@
+window.fbAsyncInit = () => {
+  FB.init({
+    appId      : '1892529307675311', //Tu APP ID
+    cookie     : true,  // habilita las cookies para que el server puede acceder a la sesion
+    xfbml      : true,  // parsea los plugins sociales en la pagina
+    version    : 'v2.8' // usa version 2.8
+  });
+
+  FB.getLoginStatus((response) => {
+    loginHandler(response);
+  });
+};
+
+function loginHandler(response) {
+  if (response.status === 'connected') {
+    state.status = "Conectado";
+    FB.api('/me?fields=email,name', user => {
+      state.user = user;
+      state.doRender();
+    });
+  } else if (response.status === 'not_authorized') {
+    state.user = null;
+    state.status = "Aplicación no autorizada";
+    state.doRender();
+  } else {
+    state.user = null;
+    state.doRender();
+  }
+}
+
+function doLogin() {
+  FB.login(loginHandler, {scope: 'email'});
+}
